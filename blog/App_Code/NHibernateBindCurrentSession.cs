c3245f8d89bdb988ad86a2f1991a24002574045a
whongxiao <whongxiao_88@163.com>
2017-05-25 16:23:46 +0800

    Add HttpModule
    add httpModule  NHibernateBindCurrentSession

---
+++ blog/App_Code/NHibernateBindCurrentSession.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using NHibernate;
+using NHibernate.Context;
+using NHibernateUtility;
+/// <summary>
+/// NHibernateBindCurrentSession 的摘要说明
+/// </summary>
+public class NHibernateBindCurrentSession:IHttpModule
+{
+    public void Init(HttpApplication context)
+    {
+        context.BeginRequest += new EventHandler(Application_BeginRequest);
+        context.EndRequest += new EventHandler(Application_EndRequest);
+    }
+    public void Dispose()
+    {
+    }
+    private void Application_BeginRequest(object sender, EventArgs e)
+    {
+        log4net.Config.XmlConfigurator.Configure();
+        //ISession session = NHibernateSessionHelper.OpenSession("Model");
+        ISession session = NHibernateSessionHelper.GetCurrentSession();
+        // CurrentSessionContext.Bind(session);
+    }
+    private void Application_EndRequest(object sender,EventArgs e)
+    {
+        ISession session = CurrentSessionContext.Unbind(NHibernateSessionHelper.SessionFactory);
+        if(session!=null)
+            try
+            {
+                if(session.IsOpen==true)
+                    if(session.IsConnected==true&&session.Transaction.IsActive==true)
+                    session.Transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Transaction.Rollback();
+            }
+            finally
+            {
+                if (session.IsOpen == true)
+                    session.Close();
+            }
+    }
+ 
+}
